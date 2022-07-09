171dd2c369efff0bd46f3fdbe33006553870083f
xiangnan <280145668@qq.com>
2022-07-09 11:04:44 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import ValidateInput from './components/ValidateInput.vue'
+import packages from '../package.json'
+</script>
+
+<template>
+  <h1>{{ packages.name }}-{{packages.version}}</h1>
+  <ValidateInput msg="Validator" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
